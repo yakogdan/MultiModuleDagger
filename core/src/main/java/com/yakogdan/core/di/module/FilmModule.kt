5c5b417c2beda76100486d7c8b46c06636b1f9b5
Bogdan <yakogdan@gmail.com>
2025-10-02 21:42:43 +0800

    В FilmModule использован CoreScope

--- core/src/main/java/com/yakogdan/core/di/module/FilmModule.kt
+++ core/src/main/java/com/yakogdan/core/di/module/FilmModule.kt
@@ -3,6 +3,7 @@ package com.yakogdan.core.di.module
 import com.yakogdan.core.data.api.kinopoisk.KinopoiskApiFactory
 import com.yakogdan.core.data.api.kinopoisk.KinopoiskApiService
 import com.yakogdan.core.data.repository.FilmRepoImpl
+import com.yakogdan.core.di.scope.CoreScope
 import com.yakogdan.core.domain.repository.FilmRepository
 import dagger.Binds
 import dagger.Module
@@ -11,12 +12,12 @@ import dagger.Provides
 @Module
 internal interface FilmModule {
 
-    @Binds
+    @[Binds CoreScope]
     fun bindFilmRepository(repoImpl: FilmRepoImpl): FilmRepository
 
     companion object {
 
-        @Provides
+        @[Provides CoreScope]
         fun provideKinopoiskApiService(): KinopoiskApiService =
             KinopoiskApiFactory.kinopoiskApiService
     }
